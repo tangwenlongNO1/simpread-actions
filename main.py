7fa462c55899cc0fcd59ca2d77385b68bd986bb5
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2023-03-18 14:36:10 +0800

    Create

---
+++ main.py
@@ -0,0 +1,29 @@
+import requests
+import os
+from datetime import datetime
+
+now = datetime.utcnow().strftime('%Y/%m/%d')
+
+def sendTelegram(urls):
+    text = f'''
+    ▎ Github Actions | 刷步数 · 每日推送 {now}
+
+    {urls}
+
+    来自 [Github Action](https://github.com/tangwenlongNO1/mimotion/actions)'''
+    
+    data = {
+      "chat": os.environ.get('TELEGRAM_CHAT'),
+      "token": os.environ.get('TELEGRAM_TOKEN'),
+      "text": text.replace('{{urls}}', "n".join(urls))
+    }
+    
+    response = requests.post('https://api-wrap.simpread.pro/api/service/telegram', data=data)
+    
+    if response.status_code == 200:
+        print('sendTelegram success', response)
+    else:
+        print('sendTelegram error', response)
+    
+urls = ["www.baidu.com"]
+sendTelegram(urls)
