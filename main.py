ac8e53824c009a29b5b05c253eb7e7c8b416c70b
tangwenlongNO1 <106858241+tangwenlongNO1@users.noreply.github.com>
2023-03-18 14:48:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -17,6 +17,8 @@ def sendTelegram(urls):
       "token": os.environ.get('TELEGRAM_TOKEN'),
       "text": text.replace('{{urls}}', "n".join(urls))
     }
+    response = requests.post('https://api-wrap.simpread.pro/', data=data)
+
     if response.status_code == 200:
         print('sendTelegram success', response)
     else:
